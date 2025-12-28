a074c97c92f0550e2be49a6028c82586ed667889
umeshboda2024 <umeshboda2024.katargam@gmail.com>
2025-12-28 22:38:43 +0530

    added2

--- src/Component/Stateporpertycard.jsx
+++ src/Component/Stateporpertycard.jsx
@@ -1,34 +1,58 @@
-import { Box, Container, Grid } from '@mui/material';
-import React from 'react';
-import Ahemdabadimage from "../Assets/images/Ahemdabadstate.jpg"
+import { Box, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import Ahemdabadimage from "../Assets/images/Ahemdabadstate.jpg";
+import Gurugram from "../Assets/images/Gurgamstate.jpg";
+import Mumbai from "../Assets/images/Mumbaistate.jpg";
+import Pune from "../Assets/images/Punestate.jpg"
+
 const Stateporpertycard = () => {
+  const Stateproperty = [
+    { image: Ahemdabadimage, name: "Ahmedabad" , Project:"15"},
+    { image:  Gurugram, name: "Gururgram",Project:"10" },
+    { image: Mumbai, name: "Mumbai" ,Project:"8"},
+    { image: Pune, name: "Pune" ,Project:"2"},
+  ];
 
-    const Stateproperty =[
-        {image: Ahemdabadimage}
-    ]
   return (
-    <div>
-      <Box>
-        <Container>
-            <Grid>
-                <Grid>
-                    { Stateproperty.map((state,index) =>{
-                        <Box  
-                          component="img"
-                          src={state.image}
-                        >
+    <Box py={4} mt={5} >
+      <Container>
+        <Grid container spacing={3}>
+          {Stateproperty.map((state, index) => (
+            <Grid item size={3} xs={12} sm={6} md={4} key={index}>
+              <Box
+                component="img"
+                src={state.image}
+                alt={state.name}
+                sx={{
+                  width: "100%",
+                  height: 220,
+                  objectFit: "cover",
+                  borderRadius: "50%",
+                }}
+              />
 
-                        </Box>
-                        
-                    })
-
-                    }
-                </Grid>
+              <Typography
+                variant="h6"
+                align="center"
+                mt={1}
+                fontWeight={600}
+              >
+                {state.name}
+              </Typography>
+                  <Typography
+                variant="h6"
+                align="center"
+                mt={1}
+                fontWeight={300}
+              >
+                {state.Project} Projects
+              </Typography>
             </Grid>
-        </Container>
-      </Box>
-    </div>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
   );
-}
+};
 
 export default Stateporpertycard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,14 @@ import Hero from '../Home/Hero'
 import StateProperties from '../Home/StateProperties'
 import Buyproperties from '../Home/Buyproperties'
 import Propertycard from '../Component/Propertycard'
-import Stateporpertycard from '../Component/Stateporpertycard'
+import Stateporpertycard from "../Component/Stateporpertycard"
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
-      <Stateporpertycard/>        
+      <Stateporpertycard/>       
       <Propertycard/>
       {/* <Buyproperties></Buyproperties>  */}
     </div>
